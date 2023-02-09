a0750f0b26cfad6fe9f2f96a01007170db179d3b
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-02-08 22:13:42 -0300

    added button in Header

--- src/components/Outlet/Header/styles.ts
+++ src/components/Outlet/Header/styles.ts
@@ -4,8 +4,8 @@ export const Container = styled.div`
   width: 100%;
   max-width: 100vw;
   height: 7vh;
-  background-color: cyan;
   display: flex;
   align-items: center;
   justify-content: space-around;
+  background-color: ${props => props.theme.colors.header};
 `;
