43c0723d9f607472ed02688e659d256c89d3e6c7
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-02-08 21:50:13 -0300

    feat(themes): added dark theme and light theme and interface

---
+++ src/styles/theme/dark.ts
@@ -0,0 +1,10 @@
+export default {
+	title:'dark',
+
+	colors:{
+		primary:'#000000',
+		secundary:'',
+		header:'#000000',
+		footer:'#000000'
+	}
+};
---
+++ src/styles/theme/light.ts
@@ -0,0 +1,10 @@
+export default {
+	title:'dark',
+
+	colors:{
+		primary:'#f5f5f5',
+		secundary:'',
+		header:'#000000',
+		footer:'#000000'
+	}
+};
