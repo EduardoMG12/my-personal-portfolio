a8b3257277c3b7d7dfe0bbf09a2bd297cdec2dfb
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-02-10 15:36:49 -0300

    refactoring(App): refactoring for hook implementation, removed states from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import GlobalStyle from '@styles/globalStyle';
 import { Route, Routes } from 'react-router-dom';
 import Outlet from '@components/Outlet';
 import Home from '@pages/Home';
-import { ThemeProvider } from 'styled-components';
-import light from '@styles/themes/light';
-import dark from '@styles/themes/dark';
+import ThemeContextProvider from '@hooks/ThemeProvider';
 
 const App: React.FC = () => {
-	const [theme, setTheme] = useState(light);
-
-	const toggleTheme = () => {
-		setTheme(theme.title === 'light' ? dark : light);
-	};
-
 	return (
 		<>
-			<ThemeProvider theme={theme}>
+			<ThemeContextProvider>
 				<GlobalStyle />
 				<Routes>
-					<Route path="/" element={<Outlet toggleTheme={toggleTheme}/>} >
+					<Route path="/" element={<Outlet/>} >
 						<Route index element={<Home />} />
 					</Route>
 				</Routes>
-			</ThemeProvider>
+			</ThemeContextProvider>
 		</>
 	);
 };
