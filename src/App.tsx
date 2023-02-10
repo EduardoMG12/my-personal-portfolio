ff2f181f17f0147c5735c09c980774cd7bbbfd1d
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-02-10 15:03:18 -0300

    feat(): added state set and themes from app and added ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GlobalStyle from '@styles/globalStyle';
 import { Route, Routes } from 'react-router-dom';
 import Outlet from '@components/Outlet';
 import Home from '@pages/Home';
+import { ThemeProvider } from 'styled-components';
+import light from '@styles/themes/light';
+import dark from '@styles/themes/dark';
 
 const App: React.FC = () => {
+	const [theme, setTheme] = useState(light);
+
+	const toggleTheme = () => {
+		setTheme(theme.title === 'light' ? dark : light);
+	};
+
 	return (
 		<>
-			<GlobalStyle />
-			<Routes>
-				<Route path="/" element={<Outlet />} >
-					<Route index element={<Home />} />
-				</Route>
-			</Routes>
+			<ThemeProvider theme={theme}>
+				<GlobalStyle />
+				<Routes>
+					<Route path="/" element={<Outlet toggleTheme={toggleTheme}/>} >
+						<Route index element={<Home />} />
+					</Route>
+				</Routes>
+			</ThemeProvider>
 		</>
 	);
 };
