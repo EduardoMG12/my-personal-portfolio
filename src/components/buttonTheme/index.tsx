1d9855a869e5a5233c9d24ac806f8d66e56d5b2f
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-02-10 18:35:18 -0300

    refactor(): refactoring code added nullish coalescing  and remove fragment

--- src/components/buttonTheme/index.tsx
+++ src/components/buttonTheme/index.tsx
@@ -3,18 +3,12 @@ import React, { useContext } from 'react';
 import { useTheme } from 'styled-components';
 import { Button } from './styles';
 
-
-
 const ButtonTheme: React.FC = () => {
 	const { toggleTheme } = useContext(ToggleThemeContext);
-    
 	const theme = useTheme();
 	const isDark = theme.title === 'dark' ? true : false; 
-
 	return (
-		<>
-			<Button type="checkbox" checked={isDark} onChange={toggleTheme}/>
-		</>
+		<Button type="checkbox" checked={isDark} onChange={toggleTheme}/>
 
 	);
 };
