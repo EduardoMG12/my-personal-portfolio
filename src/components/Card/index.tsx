65397851dd9855a8d8e37aca0e5d247019719a8b
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-02-13 16:47:44 -0300

    feat(components): created component card generic

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
-
-import { Container } from './styles';
-
-export interface ICardProps {
+import { Container, Wrapper } from './styles';
+export interface IPropsCard {
     children: React.ReactNode
     wd?: string
     hg?: string
     flexD?: string
+    justifyC?: string
     bgdImage?: string
     bgdColor?: string
+    borderR?: string
 }
 
-export const Card: React.FC<ICardProps> = ({children, wd, hg, flexD, bgdImage, bgdColor}) => {
+export const Card: React.FC<IPropsCard> = ({children, wd, hg, flexD, justifyC, borderR, bgdImage, bgdColor}) => {
 	return(
-		<Container wd={wd} hg={hg} flexD={flexD} bgdImage={bgdImage} bgdColor={bgdColor}>
+		<Container wd={wd} hg={hg} borderR={borderR} flexD={flexD} justifyC={justifyC} bgdImage={bgdImage} bgdColor={bgdColor}>
 			{children}
 		</Container>
+	);
+};
+
+interface IPropsContent {
+    children: React.ReactNode
+}
+
+export const Content: React.FC<IPropsContent> = ({children}) => {
+	return( 
+		<Wrapper>
+			{children}
+		</Wrapper>
 	);
 };
