8f06f98327f3cab4a5179dd6e7cf8a4c52ad01b9
Eduardo Mello <102185386+EduardoMG12@users.noreply.github.com>
2023-02-14 16:02:05 -0300

    feat(): responsive at component Card and childrens (#4)

--- src/pages/Home/components/Icon/styles.ts
+++ src/pages/Home/components/Icon/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Li = styled.div`
-   background-color: ${props => props.theme.colors.iconsBgdCard};
+    background-color: ${props => props.theme.colors.iconsBgdCard};
     display: flex;
     justify-content: center;
     align-items: center;
@@ -25,4 +25,10 @@ export const Li = styled.div`
         
     }
     transition: 0.4s ease-in-out all;
-    `;
+    @media (max-width:768px){
+        width: 6rem;
+        height: 6rem;
+        font-size: 2.5rem;
+    }
+    `
+    ;
