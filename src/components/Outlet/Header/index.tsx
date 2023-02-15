0ee50ccfbfa497dc4ce1f5c8355c4dfd7f995775
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-02-15 00:50:30 -0300

    feat(header):created standard header layout implemented the hamburger and added the other stylizations

--- src/components/Outlet/Header/index.tsx
+++ src/components/Outlet/Header/index.tsx
@@ -1,13 +1,25 @@
 import ButtonTheme from '@components/buttonTheme';
+import Hamburguer from '@components/Hamburguer';
 import React from 'react';
 
 import { Container } from './styles';
 
 const Header: React.FC = () => {
 	return(
-		<Container>
-			<ButtonTheme />
-		</Container>
+		<>
+			<Container>
+				<ButtonTheme />
+				<h1>Charles Eduardo</h1>
+				<nav>
+					<ul>
+						<li>sobre mim</li>
+						<li>projetos</li>
+						<li><i className='fa-sharp fa-solid fa-user-tie'></i></li>
+					</ul>  
+				</nav>
+			</Container>
+			<Hamburguer/>
+		</>
 	);
 };
 
--- src/components/Outlet/Header/styles.ts
+++ src/components/Outlet/Header/styles.ts
@@ -1,11 +1,52 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 100%;
-  max-width: 100vw;
-  height: 7vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  background-color: ${props => props.theme.colors.header};
+    width: 100%;
+    max-width: 100vw;
+    height: 7vh;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: ${props => props.theme.colors.header};
+    color: ${props => props.theme.colors.headerText};
+    & h1{
+        font-size: 1.8rem;
+    }
+    & nav ul{
+        display: flex;
+        gap: 1.5rem;
+        justify-content: center;
+        align-items: center;
+        & li:nth-child(3){
+            margin-left: 2rem;
+        }
+        & li{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-size: 1.8rem;
+            cursor: pointer;
+            transition: 0.2s ease-in-out color;
+            transition: 0.5s ease-in-out transform;
+            &:hover{
+                color: ${props => props.theme.colors.headerTextHover};
+                transform: scale(1.04);
+            }
+        }
+    }
+    @media (max-width:425px) {
+        & nav ul{
+        display: none;
+        visibility: hidden;
+        cursor: none;
+        flex-direction: column;
+        gap: 0rem;
+        justify-content: flex-end;
+        align-items: center;
+        & li:nth-child(3){
+            margin-left: 0;
+        }
+    }
+    }
 `;
