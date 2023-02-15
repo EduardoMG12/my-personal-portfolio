76d7dfd8dc66c2ffae1e626f06d6173ea223d215
Eduardo Mello <102185386+EduardoMG12@users.noreply.github.com>
2023-02-15 16:05:20 -0300

    refactoring(): added styles from footer (#6)

--- src/components/Outlet/Footer/styles.ts
+++ src/components/Outlet/Footer/styles.ts
@@ -1,11 +1,29 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    overflow-y: hidden;
   width: 100%;
   max-width: 100vw;
-  height: 3vh;
-  background-color: cyan;
+  height: 4vh;
+  background-color:${props => props.theme.colors.header};
   display: flex;
   align-items: center;
-  justify-content: space-around;
+  justify-content: flex-start;
+  padding-left: 2rem;
+  border-top: 0.1rem solid ${props => props.theme.colors.borderHeader};
+  & h2{
+    height: 100%;
+  }
+  & h2, a{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    font-size: 2rem;
+    color: ${props => props.theme.colors.headerText};
+  }
+  & a{
+    border-bottom: 0.1rem solid ${props => props.theme.colors.headerText};
+    margin-inline: 0.5rem;
+  }
 `;
