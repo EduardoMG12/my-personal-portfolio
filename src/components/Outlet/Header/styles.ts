714b426696c513dc3cc27fe4d09e50024b06c2e9
Charles Eduardo Mello Guimaraes <charles.eduardo.mello.guimaraes@hotmail.com>
2023-02-16 00:29:19 -0300

    fixed() others styles

--- src/components/Outlet/Header/styles.ts
+++ src/components/Outlet/Header/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import 'typeface-open-sans';
+
 export const Container = styled.div`
     @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
 
